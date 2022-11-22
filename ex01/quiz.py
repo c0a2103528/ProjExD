f88c6fefb1e981d8b12a901254daed85854aa169
Hayato <c0A2103528@edu.teu.ac.jp>
2022-11-22 15:37:18 +0900

    quiz.py ver1.2

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,35 +1,34 @@
 from random import randint
 import datetime
-"""
+
 def main():
-    answers = [["ますお", "マスオ"],
-            ["わかめ", "ワカメ"],
-            ["甥", "おい", "甥っ子", "おいっこ"]]
-    quizzes = ["サザエの旦那の名前は？",
-            "カツオの妹の名前は？",
-            "タラオはカツオから見てどんな関係？"]
-
-    def shutudai(q):
+    def shutudai(q, qn):
         print("問題：")
-        quiz_num = randint(0, 2)
-        print(q[quiz_num])
-        return quiz_num
+        print(q[qn])
+        st = datetime.datetime.now()
+        ans = input("答えるんだ：")
+        ed = datetime.datetime.now()
+        return ans, (ed-st).seconds
 
-    def kaitou(answer, ans, i):
+    def kaitou(answer, ans, i, sec):
         if answer in ans[i]:
             print("正解！！！")
         else:
             print("出直してこい")
+        print(f"解答時間：{sec}秒")
 
-    q_num = shutudai(quizzes)
+    answers = [["ますお", "マスオ"],
+            ["わかめ", "ワカメ"],
+            ["甥", "おい", "甥っ子", "おいっこ"]]
 
-    st = datetime.datetime.now()
-    ans = input("答えるんだ：")
-    ed = datetime.datetime.now()
+    quizzes = ["サザエの旦那の名前は？",
+            "カツオの妹の名前は？",
+            "タラオはカツオから見てどんな関係？"]
 
-    kaitou(ans, answers, q_num)
-    sec = (ed - st).seconds
-    print(f"解答時間：{sec}秒")
+    quiz_num = randint(0, 2)
+    ans, sec = shutudai(quizzes, quiz_num)
+
+    kaitou(ans, answers, quiz_num, sec)
 
 """
 def main():
@@ -54,7 +53,12 @@ def main():
         print("出直してこい")
 
     sec = (ed - st).seconds
-    print(f"解答時間：{sec}秒")
-
+    if sec < 60:
+        print(f"解答時間：{sec}秒")
+    else:
+        min = sec // 60
+        sec -= 60 * min
+        print(f"解答時間：{min}分{sec}秒")
+"""
 
 main()
