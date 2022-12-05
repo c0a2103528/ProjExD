f479792a8333721c29d49c982fa0eb8ee18e5196
Hayato <c0A2103528@edu.teu.ac.jp>
2022-12-05 14:03:28 +0900

    2nd commit

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,17 +1,37 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up(event):
+    global key
+    key = ""
+
+def main_proc():
+    global cx, cy, key
+    if key == "Up":
+        cy -= 20
+    elif key == "Down":
+        cy += 20
+    elif key == "Left":
+        cx -= 20
+    elif key == "Right":
+        cx += 20
+    canvas.coords("tori", cx, cy)
+    root.after(100, main_proc)
+
 root = tk.Tk()
 root.title("迷えるこうかとん")
 root.geometry("1500x900")
+root.bind("<KeyPress>", key_down)
+root.bind("<KeyRelease>", key_up)
 
 canvas = tk.Canvas(root, width=1500, height=900, bg = "black")
-
-cx = 300
-cy = 400
+canvas.pack()
+cx = 300; cy = 400
 tori = tk.PhotoImage(file="ex03/fig/6.png")
 canvas.create_image(cx, cy, image=tori, tag="tori")
-canvas.pack()
-
 key=""
-
+main_proc()
 root.mainloop()
