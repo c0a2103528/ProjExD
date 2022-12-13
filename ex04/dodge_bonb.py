e048128668285e5a2ca7614dc5ea7954e6eed738
Hayato <c0A2103528@edu.teu.ac.jp>
2022-12-13 11:34:47 +0900

    3rd, 4th

--- ex04/dodge_bonb.py
+++ ex04/dodge_bonb.py
@@ -11,13 +11,33 @@ def main():
 
     clock = pg.time.Clock()
 
+    pg_x = 900; pg_y = 400
+
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-        pg.display.update()
         clock.tick(1000)
+        pg_sfc = pg.image.load("fig/9.png")
+        pg_sfc = pg.transform.rotozoom(pg_sfc, 0, 2.0)
+        pg_rct = pg_sfc.get_rect()
+        pg_rct.center = pg_x, pg_y
+        scrn_sfc.blit(pg_sfc, pg_rct)
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            pg_y -= 1
+        elif key_lst[pg.K_DOWN]:
+            pg_y += 1
+        elif key_lst[pg.K_LEFT]:
+            pg_x -= 1
+        elif key_lst[pg.K_RIGHT]:
+            pg_x += 1
+
+        pg.display.update()
+
+
 
 
 if __name__ == "__main__":
