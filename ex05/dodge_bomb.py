9411be25d59f8e0aa571d13ac90dc3136343470a
Hayato <c0A2103528@edu.teu.ac.jp>
2022-12-20 14:10:12 +0900

    2nd

--- ex05/dodge_bomb.py
+++ ex05/dodge_bomb.py
@@ -3,7 +3,6 @@ import random
 import sys
 
 class Screen:
-
     def __init__(self, title, wh, file):
         pg.display.set_caption(title)
         self.sfc = pg.display.set_mode(wh)
@@ -14,19 +13,37 @@ class Screen:
     def blit(self):
         self.sfc.blit(self.bgi_sfc, self.bgi_rct)
 
-key_delta = {
-    pg.K_UP:    [0, -1],
-    pg.K_DOWN:  [0, +1],
-    pg.K_LEFT:  [-1, 0],
-    pg.K_RIGHT: [+1, 0],
-}
+
+class Bird:
+    key_delta = {
+        pg.K_UP:    [0, -1],
+        pg.K_DOWN:  [0, +1],
+        pg.K_LEFT:  [-1, 0],
+        pg.K_RIGHT: [+1, 0],}
+
+    def __init__(self, fig, rate, xy):
+        self.sfc = pg.image.load("fig/6.png")
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, rate)
+        self.rct = self.sfc.get_rect()
+        self.rct.center = xy
+    
+    def blit(self, scn):
+        scn.sfc.blit(self.sfc, self.rct)
+    
+    def update(self, scr):
+        key_dct = pg.key.get_pressed()
+        for key, delta in self.key_delta.items():
+            if key_dct[key]:
+                self.rct.centerx += delta[0]
+                self.rct.centery += delta[1]
+
+            if check_bound(self.rct, scr.rct) != (+1, +1):
+                self.rct.centerx -= delta[0]
+                self.rct.centery -= delta[1]
+        self.blit(scr)
+
 
 def check_bound(obj_rct, scr_rct):
-    """
-    第1引数：こうかとんrectまたは爆弾rect
-    第2引数：スクリーンrect
-    範囲内：+1／範囲外：-1
-    """
     yoko, tate = +1, +1
     if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
         yoko = -1
@@ -41,12 +58,9 @@ def main():
     SR = Screen("逃げろ！こうかとん", (1600, 900), "fig/pg_bg.jpg")
 
     # 練習３
-    tori_sfc = pg.image.load("fig/6.png")
-    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
-    tori_rct = tori_sfc.get_rect()
-    tori_rct.center = 900, 400
+    tori = Bird("fig/0.png", 2.0, (900, 400))
     # sfcにtori_rctに従って，tori_sfcを貼り付ける
-    SR.sfc.blit(tori_sfc, tori_rct) 
+    SR.sfc.blit(tori.sfc, tori.rct) 
 
     # 練習５
     bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
@@ -66,16 +80,7 @@ def main():
             if event.type == pg.QUIT:
                 return
 
-        key_dct = pg.key.get_pressed()
-        for key, delta in key_delta.items():
-            if key_dct[key]:
-                tori_rct.centerx += delta[0]
-                tori_rct.centery += delta[1]
-            # 練習7
-            if check_bound(tori_rct, SR.rct) != (+1, +1):
-                tori_rct.centerx -= delta[0]
-                tori_rct.centery -= delta[1]
-        SR.sfc.blit(tori_sfc, tori_rct) # 練習3
+        tori.update(SR)
 
         # 練習６
         bomb_rct.move_ip(vx, vy)
@@ -85,7 +90,7 @@ def main():
         vy *= tate
 
         # 練習８
-        if tori_rct.colliderect(bomb_rct):
+        if tori.rct.colliderect(bomb_rct):
             return
 
         pg.display.update()
@@ -96,51 +101,4 @@ if __name__ == "__main__":
     pg.init()
     main()
     pg.quit()
-    sys.exit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def main():
-    
-
-
-    clock = pg.time.Clock() # 練習1
-    while True:
-        sfc.blit(bg_sfc, bg_rct) # 練習2
-        
-        for event in pg.event.get(): # 練習2
-            if event.type == pg.QUIT:
-                return
-
-
-        # 練習7
-        yoko, tate = check_bound(bomb_rct, rct)
-        vx *= yoko
-        vy *= tate
-        bomb_rct.move_ip(vx, vy) # 練習6
-        sfc.blit(bomb_sfc, bomb_rct) # 練習5
-
-        # 練習8
-        if tori_rct.colliderect(bomb_rct): # こうかとんrctが爆弾rctと重なったら
-            return
-
-        pg.display.update() #練習2
-        clock.tick(1000)
-
-if __name__ == "__main__":
-    pg.init() # 初期化
-    main() # ゲームの本体
-    pg.quit() # 初期化の解除
     sys.exit()
