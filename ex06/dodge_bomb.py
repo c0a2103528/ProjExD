57b335588faed4f987e815b3c123d09574cae37e
Hayato <c0A2103528@edu.teu.ac.jp>
2023-01-10 15:01:26 +0900

    retake

--- ex06/dodge_bomb.py
+++ ex06/dodge_bomb.py
@@ -2,116 +2,153 @@ import pygame as pg
 import random
 import sys
 
-#画面外に出たか判定
+
+# スクリーンの設定
+class Screen:
+    def __init__(self, title, wh, file):
+        pg.display.set_caption(title)
+        self.sfc = pg.display.set_mode(wh)
+        self.rct = self.sfc.get_rect()
+        self.bgi_sfc = pg.image.load(file)
+        self.bgi_rct = self.bgi_sfc.get_rect()
+    
+    def blit(self):
+        self.sfc.blit(self.bgi_sfc, self.bgi_rct)
+
+
+# 操作キャラの設定
+class Bird:
+
+    key_delta = {
+        pg.K_UP:    [0, -1],
+        pg.K_DOWN:  [0, +1],
+        pg.K_LEFT:  [-1, 0],
+        pg.K_RIGHT: [+1, 0],}
+
+    def __init__(self, fig, rate, xy):
+        self.rate = rate
+        self.sfc = pg.image.load(fig)
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, self.rate)
+        self.rct = self.sfc.get_rect()
+        self.rct.center = xy
+    
+    def blit(self, scn):
+        scn.sfc.blit(self.sfc, self.rct)
+
+    # 入力キーによって移動 
+    def update(self, scr):
+        key_dct = pg.key.get_pressed()
+        for key, delta in self.key_delta.items():
+            if key_dct[key]:
+                self.rct.centerx += delta[0]
+                self.rct.centery += delta[1]
+
+            if check_bound(self.rct, scr.rct) != (+1, +1):
+                self.rct.centerx -= delta[0]
+                self.rct.centery -= delta[1]
+        self.blit(scr)
+    
+    # 画像の変更
+    def change_image(self, fig):
+        self.sfc = pg.image.load(fig)
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, self.rate)
+
+
+# 爆弾の設定
+class Bomb:
+    def __init__(self, col, r, sp, scr):
+        self.sfc = pg.Surface((r*2, r*2))
+        self.sfc.set_colorkey((0, 0, 0))
+        pg.draw.circle(self.sfc, col, (r, r), r)
+        self.rct = self.sfc.get_rect()
+        self.rct.centerx = random.randint(0, scr.rct.width)
+        self.rct.centery = random.randint(scr.rct.height-500, scr.rct.height)
+        self.vx = sp[0]
+        self.vy = sp[1]
+    
+    def blit(self, scr):
+        scr.sfc.blit(self.sfc, self.rct)
+    
+    def update(self, scr):
+        self.rct.move_ip(self.vx, self.vy)
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
+        self.blit(scr)
+
+    def stop(self, scr):
+        self.vx = 0
+        self.vy = 0
+        self.rct.centerx = scr.rct.width + 50
+        self.rct.centery = scr.rct.height + 50
+        self.blit(scr)
+
+
+# 跳ね返りの確認
 def check_bound(obj_rct, scr_rct):
     yoko, tate = +1, +1
     if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
         yoko = -1
-    if obj_rct.top < scr_rct.top+400 or scr_rct.bottom < obj_rct.bottom:
+    if obj_rct.top < scr_rct.top + 400 or scr_rct.bottom < obj_rct.bottom:
         tate = -1
     return yoko, tate
 
+
 def main():
     clock =pg.time.Clock()
-    pg.display.set_caption("逃げろ！こうかとん")
-    scrn_sfc = pg.display.set_mode((600, 900))
-    scrn_rct = scrn_sfc.get_rect()
-
-    #背景設定
-    bg_sfc = pg.image.load("fig/bg.png")
-    bg_rct = bg_sfc.get_rect()
+    # スクリーンの表示
+    SR = Screen("戦え！こうかとん", (600, 900), "fig/bg.png")
     gd_sfc = pg.image.load("fig/gd.png")
     gd_rct = gd_sfc.get_rect()
-    gd_rct.center = scrn_rct.right/2, scrn_rct.bottom-250
+    gd_rct.center = SR.rct.right/2, SR.rct.bottom-250
 
-    #こうかとんの設定
-    tori_sfc1 = pg.image.load("fig/9.png")      #陸にいるとき
-    tori_sfc1 = pg.transform.rotozoom(tori_sfc1, 0, 2.0)
-    tori_sfc2 = pg.image.load("fig/3.png")      #空にいるとき
-    tori_sfc2 = pg.transform.rotozoom(tori_sfc2, 0, 2.0)
-    tori_sfc3 = pg.image.load("fig/10.png")      #爆弾に当たった時
-    tori_sfc3 = pg.transform.rotozoom(tori_sfc3, 0, 2.0)
-    tori_rct = tori_sfc1.get_rect()
-    tori_rct.center = 200, 600
-    scrn_sfc.blit(tori_sfc1, tori_rct)
+    # 操作キャラ、剣表示
+    tori = Bird("fig/6.png", 2.0, (200, 600))
+    tori.update(SR)
 
-    #爆弾の設定
-    bomb_sfc = pg.Surface((20, 20))
-    bomb_sfc.set_colorkey((0, 0, 0))
-    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
-    bomb_rct = bomb_sfc.get_rect()
-    bomb_rct.centerx = random.randint(0, scrn_rct.width)
-    bomb_rct.centery = random.randint(scrn_rct.height-500, scrn_rct.height)
-    scrn_sfc.blit(bomb_sfc, bomb_rct)
-
-    #爆弾の初期移動方向の設定
-    move_bomb = [-1, 1]
-    vx = move_bomb[random.randint(0, 1)]
-    vy = move_bomb[random.randint(0, 1)]
+    #爆弾の色設定、進行方向の設定、表示
+    colors = ["Red", "Blue", "Green", "Yellow", "Purple"]
+    bombs = []
+    num = 1
+    for i in range(num):
+        vx = random.choice([-1, 1])
+        vy = random.choice([-1, 1])
+        color = colors[i % 5]
+        bombs.append(Bomb(color, 10, (vx, vy), SR))
+        bombs[i].update(SR)
 
     while True:
-        scrn_sfc.blit(bg_sfc, bg_rct)
-        scrn_sfc.blit(gd_sfc, gd_rct)
+        SR.blit()
+        SR.sfc.blit(gd_sfc, gd_rct)
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-
-        #経過時間を表示
-        sec = pg.time.get_ticks()
-        fonto = pg.font.Font(None, 80)
-        txt = fonto.render(f"{sec/1000:.2f}", True, "BLACK")
-        scrn_sfc.blit(txt, (800, 30))
-
-        #こうかとんの移動
-        key_dct = pg.key.get_pressed()
-        bf_x = tori_rct.centerx
-        bf_y = tori_rct.centery
-        if key_dct[pg.K_UP]:
-            tori_rct.centery -= 1
-        if key_dct[pg.K_DOWN]:
-            tori_rct.centery += 1
-        if key_dct[pg.K_LEFT]:
-            tori_rct.centerx -= 1
-        if key_dct[pg.K_RIGHT]:
-            tori_rct.centerx += 1
-        if check_bound(tori_rct, scrn_rct) != (+1, +1):
-            tori_rct.centerx = bf_x
-            tori_rct.centery = bf_y
-        scrn_sfc.blit(tori_sfc1, tori_rct)
-
-        #爆弾の移動
-        bomb_rct.move_ip(vx, vy)
-        scrn_sfc.blit(bomb_sfc, bomb_rct) 
-        yoko, tate = check_bound(bomb_rct, scrn_rct)
-        vx *= yoko
-        vy *= tate
         
-        """
-        #時間経過による爆弾の速度変化
-        change_speed = 0.001
-        if vx > 0:
-            vx += change_speed
-        else:
-            vx -= change_speed
-        if vy > 25
-            vy += change_speed
-        else:
-            vy -= change_speed
+        # 操作キャラ・剣の位置更新
+        tori.update(SR)
 
-        #こうかとんとボールが重なった時
-        if tori_rct.colliderect(bomb_rct):
-            #泣いている画像に変更
-            scrn_sfc.blit(bg_sfc, bg_rct)
-            scrn_sfc.blit(tori_sfc3, tori_rct)
+        # 陸上・空中によって画像変更
+        if (tori.rct.centery < 450):
+            tori.change_image("fig/3.png")
+        elif (tori.rct.centery >= 450):
+            tori.change_image("fig/5.png")
 
-            #2秒の遅延後、終了する
-            clock.tick(0.5)
-            return
-        """
+        # ゲームオーバーの設定
+        for bomb in bombs:
+            bomb.update(SR)
+            if tori.rct.colliderect(bomb.rct):
+                SR.blit()
+                tori.change_image("fig/10.png")
+                tori.update(SR)
+                pg.display.update()
+
+                clock.tick(0.5)
+                return
 
         pg.display.update()
         clock.tick(1000)
+
 
 if __name__ == "__main__":
     pg.init()
